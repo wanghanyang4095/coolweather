bf3d2b80f9a099c07fd4ef12ac6650655d9a6a45
Tony <wanghanyang4095@126.com>
2015-10-21 18:46:20 +0800

    完成遍历省市县三级列表的功能。

--- src/com/coolweather/app/model/CoolWeatherDB.java
+++ src/com/coolweather/app/model/CoolWeatherDB.java
@@ -89,7 +89,7 @@ public class CoolWeatherDB {
 	 */
 	public List<City> loadCities(int provinceId){
 		List<City> list = new ArrayList<City>();
-		Cursor cursor = db.query("City", null, "provice_id=?", 
+		Cursor cursor = db.query("City", null, "province_id=?", 
 				new String[]{String.valueOf(provinceId)}, null, null, null);
 		if(cursor.moveToFirst()){
 			do{
